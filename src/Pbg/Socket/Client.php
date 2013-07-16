178e24dba1bad6c78f6dcfd0b0423b17581d79c1
Piotr Bogdan <ppbogdan@gmail.com>
2013-07-16 20:56:45 +0100

    First draft of the client.

---
+++ src/Pbg/Socket/Client.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace Pbg\Socket;
+
+class Client
+{
+    use Configurable;
+
+    protected $socket = null;
+
+    /**
+     * Available options:
+     *
+     * - protoFamily
+     * - socketType
+     * - socketProtocol
+     * - socketTimeout
+     * - blocking
+     * @param $options array
+     */
+    public function __construct($options = array())
+    {
+        if (sizeof($options) > 0) {
+            $this->setOptions($options);
+        }
+    }
+
+    public function connect()
+    {
+        $this->socket = $this->createSocket();
+
+        try {
+            $port = $this->getPort();
+        } catch(\Exception $e) {
+            $port = 0;
+        }
+
+        $r = @socket_connect(
+            $this->socket,
+            $this->getHost(),
+            $port
+        );
+
+        if ($r === false && $this->getSocketProtocol() !== "udp") {
+            $this->throwSocketException();
+        }
+    }
+
+    public function send($buffer)
+    {
+        if ($this->socket === null) {
+            $this->connect();
+        }
+
+        $r = @socket_send(
+            $this->socket,
+            $buffer,
+            strlen($buffer)
+        );
+
+        if ($r === false) {
+            $this->throwSocketException();
+        }
+
+        return $r;
+    }
+
+    public function receive()
+    {
+        $buffer = "";
+
+        while ($r = socket_read($this->socket, 2048) !== false) {
+            $buffer .= $r;
+        }
+
+        return $buffer;
+    }
+
+    protected function createSocket()
+    {
+        $s = socket_create(
+            $this->getProtoFamily(),
+            $this->getSocketType(),
+            $this->getSocketProtocol()
+        );
+
+        if ($s === false) {
+            $this->throwSocketException();
+        }
+
+        return $s;
+    }
+
+    protected function throwSocketException()
+    {
+        throw new \Exception(
+            sprintf("Can't create socket: %s",
+                    socket_strerror(socket_last_error($this->socket))
+                   )
+        );
+    }
+}
